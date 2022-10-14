c8f2624c0fc197c6530221ef26dc0ece3c95ba20
Marco Magdy <marcomag@apple.com>
2022-10-14 14:24:24 -0700

    Add format-checking to CI

---
+++ format-check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CLANG_FORMAT=clang-format
+
+if ! type $CLANG_FORMAT > /dev/null 2>&1; then
+    echo "No appropriate clang-format found."
+    exit 1
+fi
+
+FAIL=0
+SOURCE_FILES=$(find . -type f -name "*.h" -o -name "*.cpp")
+for i in $SOURCE_FILES
+do
+    if [ $($CLANG_FORMAT -output-replacements-xml $i | grep -c "<replacement ") -ne 0 ]
+    then
+        echo "$i failed clang-format check."
+        FAIL=1
+    fi
+done
+
+exit $FAIL
+
